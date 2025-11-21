c7146c7ea895f0a4aefed4f9cd88fbe3e97e447f
Chukwudi Chima <brianchima22@gmail.com>
2025-11-22 00:08:24 +0100

    fixed cors error, allowed all url origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from api.v1 import employees, plans
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Gym Web App API",
     version="1.0.0",
     description="Backend API for gym coaches, nutritionists, and workout plans."
+)
+
+# frontend allowed origin url
+# origins = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000"
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #allow all url origins
+    # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include Routers
